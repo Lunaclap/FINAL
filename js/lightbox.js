c09f3fc7f8ac302a0bdf261bdc08bd51a46ec7e4
Lunaclap <ruthlunabobadilla@gmail.com>
2022-12-08 12:15:56 -0500

    JS no me sale

---
+++ js/lightbox.js
@@ -0,0 +1,34 @@
+/*      no me sale :,)      */
+
+/*const imagenes = document.querySelectorAll('.img-galeria')
+
+const imagenesLight = document.querySelectorAll('.agregar-imagen')
+
+const contenedeorLight = document.querySelectorAll('.img-light')
+
+
+
+imagenes.forEach(imagen =>{
+
+    imagen.addEventListener('click',()=>{
+
+        aparecerImagen(imagen.getAttribute('src'))
+
+    })
+
+})
+
+
+
+
+const aparecerImagen=(imagen)=>{
+
+    imagenesLight.src=imagen
+
+    contenedeorLight.classList.toggle('show')
+
+    imagenesLight.classList.toggle('showImage')
+
+}
+
+*/
